a40478e86a0a9ffdfb499d2d18e4a994ee60f19e
wiktor <wiktor.owczarek245@gamil.com>
2023-01-06 15:03:37 +0100

    initial

---
+++ TAN_PD3825/Zadanie1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Zadanie1
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string websiteUrl = "https://www.pja.edu.pl/";
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(websiteUrl);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            // Adresy email / Numery telefonow
+            MatchCollection result = Regex.Matches(content, "[a-zA-Z]+[@][a-zA-Z.]+");
+            foreach (Match match in result)
+            {
+                Console.WriteLine(match);
+            }
+        }
+
+    }
+
+
+}
