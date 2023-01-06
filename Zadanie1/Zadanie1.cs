8514c3f51c05a620c1adbc7cf7d37f48603c95f5
wiktor <wiktor.owczarek245@gamil.com>
2023-01-06 15:19:52 +0100

    regex for tel number

--- Zadanie1/Zadanie1.cs
+++ Zadanie1/Zadanie1.cs
@@ -17,7 +17,8 @@ namespace Zadanie1
             string Telefony = @"\+48\s[0-9]{2}\s[0-9]{2}\s[0-9]{2}\s[0-9]{3}|" +
                                    @"\+48\s[0-9]{3}\s[0-9]{3}\s[0-9]{3}|\+48\s[0-9]{9}|" +
                                     @"[0-9]{2}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}|" +
-                                    @"[0-9]{2}\s[0-9]{2}\s[0-9]{3}\s[0-9]{2}";
+                                    @"[0-9]{2}\s[0-9]{2}\s[0-9]{3}\s[0-9]{2}"; 
+            
             // Adresy email / Numery telefonow
             MatchCollection result = Regex.Matches(content, Telefony);
             foreach (Match match in result)
